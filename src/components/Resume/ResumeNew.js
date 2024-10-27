a6f869094cc1fd65ef322d929bfbc426b432a842
Ayoub Mahmoud <116462151+ayoubxmahmoud@users.noreply.github.com>
2024-10-27 23:15:19 +0100

    Add files via upload

---
+++ src/components/Resume/ResumeNew.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Particle from "../Particle";
+import pdf from "../../Assets/../Assets/mon_cv.pdf";
+import { AiOutlineDownload } from "react-icons/ai";
+
+import { Worker, Viewer } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+
+function ResumeNew() {
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  return (
+    <div>
+      <Container fluid className="resume-section">
+        <Particle />
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+
+        <Row className="resume">
+          <Worker
+            workerUrl={`https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.worker.min.js`}
+          >
+            <Viewer fileUrl={pdf} />
+          </Worker>
+        </Row>
+
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default ResumeNew;
