169dbaa43934aa64f9f842bbd74e46cf4fe4fb97
Ayoub Mahmoud <116462151+ayoubxmahmoud@users.noreply.github.com>
2024-11-25 23:34:05 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/ayoubxmahmoud/Portfolio"
+                href="https://github.com/ayoubxmahmoud/my-portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
